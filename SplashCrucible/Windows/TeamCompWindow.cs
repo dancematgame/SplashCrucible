8565dfde0a52728c419a0783e46b42360a28003d
dancematgame <dancematgame@gmail.com>
2026-09-10 19:52:06 +0100

    Show current Crucible mode in persistent main window

--- SplashCrucible/Windows/TeamCompWindow.cs
+++ SplashCrucible/Windows/TeamCompWindow.cs
@@ -5,18 +5,28 @@ using Dalamud.Interface.Windowing;
 
 namespace SplashCrucible.Windows;
 
+public enum CrucibleMode
+{
+    Unknown,
+    TeamSelection,
+    Map,
+    Combat,
+}
+
 public sealed class TeamCompWindow : Window, IDisposable
 {
+    public CrucibleMode CurrentMode { get; set; } = CrucibleMode.Unknown;
+
     public TeamCompWindow()
-        : base("Splash Crucible##TeamComp")
+        : base("Splash Crucible##Main")
     {
         SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(220, 100),
+            MinimumSize = new Vector2(260, 110),
             MaximumSize = new Vector2(float.MaxValue, float.MaxValue),
         };
 
-        IsOpen = false;
+        IsOpen = true;
         RespectCloseHotkey = false;
     }
 
@@ -26,6 +36,16 @@ public sealed class TeamCompWindow : Window, IDisposable
 
     public override void Draw()
     {
+        var modeText = CurrentMode switch
+        {
+            CrucibleMode.TeamSelection => "Team Selection",
+            CrucibleMode.Map => "Map",
+            CrucibleMode.Combat => "Combat",
+            _ => "Unknown / Idle",
+        };
+
         ImGui.TextUnformatted("Splash Debug");
+        ImGui.Separator();
+        ImGui.TextUnformatted($"Mode: {modeText}");
     }
 }
